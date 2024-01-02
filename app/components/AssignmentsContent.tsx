de5d286777e8e564039eb48bf81c64b00e25eacb
Thomas Mithamo <enigmasoftwaredevelopers@gmail.com>
2024-01-02 23:51:59 +0300

    02/01/2024-first-upload-patch2

--- app/components/AssignmentsContent.tsx
+++ app/components/AssignmentsContent.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import NotificationStudents from './NotificationStudents'
-import AsContent from './AsContent'
+//import AsContent from './AsContent'
 
 function AssignmentsContent() {
   return (
     <div className='sm:w-full  lg:flex lg:flex-row lg:w-4/5  ' >
         <NotificationStudents/>
-        <AsContent/>
+        {/*<AsContent/>*/}
         
     </div>
   )
