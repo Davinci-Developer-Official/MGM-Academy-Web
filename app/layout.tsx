cc2fcefc56010b0c3ea38f45ebef6af7488474d6
Thomas <thomas@HP-EliteBook-8440p-66d29261.localdomain>
2024-06-10 00:59:30 +0300

    10/06/2024.7

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,15 @@
+'use client'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Providers } from './components/providers';
+import { useEffect, useState } from 'react';
+import { useHotkeys } from 'react-hotkeys-hook';
 
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
+ const metadata: Metadata = {
   title: 'MGM Academy',
   description: 'Institute of Gender and Women Empowerment',
 }
@@ -16,6 +19,17 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  const a =""
+  const [zoom, setZoom] = useState(1);
+
+  useHotkeys('ctrl+=', () => setZoom((prevZoom) => prevZoom + 0.1));
+  useHotkeys('ctrl+-', () => setZoom((prevZoom) => Math.max(0.1, prevZoom - 0.1)));
+  useHotkeys('ctrl+0', () => setZoom(1));
+
+  useEffect(() => {
+    document.documentElement.style.transform = `scale(${zoom})`;
+    document.documentElement.style.transformOrigin = '0 0';
+  }, [zoom]);
   return (
     <html lang="en">
       <body className={inter.className && 'background'}>{/*background color linear and fallback color */}
