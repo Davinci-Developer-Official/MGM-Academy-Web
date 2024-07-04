fc5216884fcc4389577a48b5d6d71e94d7c50b1c
Thomas <davincideveloper01@gmail.com>
2024-07-04 19:07:03 +0300

    04/07/2024.1

--- app/academics/(portals)/adminPortal/page.tsx
+++ app/academics/(portals)/adminPortal/page.tsx
@@ -4,6 +4,7 @@ import { FaSearch } from 'react-icons/fa';
 import placeholder from '@/public/categories/business-studies-FO8nWoT6OnZ7DXO6xYA2TnRK4kzhwt.jpg';
 import Image from 'next/image';
 import data from './data.json';
+import Rating from './Rating';
 
 export interface Courses {
   id: number;
@@ -36,21 +37,111 @@ export interface Payments {
   date: string;
 }
 
-type AllData = {
-  type: string;
-  data: Courses | Students | Instructors | Payments;
-}
-
 const Page = () => {
   const [searchParams, setSearchParams] = useState<boolean>(false);
-  const [searchParamData, setSearchParamData] = useState<AllData[]>([]);
-  const[courses,setcourses]=useState<Courses[]>([]);
-  const[instructors,setinstructors]=useState<Instructors[]>([]);
-  const[students,setstudents]=useState<Students[]>([]);
-  const[payments,setpayments]=useState<Payments[]>([]);
   const [filterOption, setFilterOption] = useState<string>('ALL');
 
+  const [courses, setCourses] = useState<Courses[]>([]);
+  const [instructors, setInstructors] = useState<Instructors[]>([]);
+  const [students, setStudents] = useState<Students[]>([]);
+  const [payments, setPayments] = useState<Payments[]>([]);
+
   useEffect(() => {
+    // Load all data initially
+    setCourses(data.courses);
+    setInstructors(data.instructors);
+    setStudents(data.students);
+    setPayments(data.payments);
+  }, []);
+
+  const filteredData = () => {
+    switch (filterOption) {
+      case 'COURSES':
+        return courses;
+      case 'STUDENTS':
+        return students;
+      case 'INSTRUCTORS':
+        return instructors;
+      case 'PAYMENTS':
+        return payments;
+      default:
+        return [];
+    }
+  };
+
+  return (
+    <div className='overflow-y-scroll h-screen'>
+      <div className='card bg-gray-200 w-[80%] p-2 mx-auto h-[540px] my-auto'>
+        <div className='h-[400px] w-[99%] mx-auto rounded-md'>
+          <p className='absolute pt-5 bg-gray-100 p-2 rounded-tl-md rounded-br-md'>Information desk</p>
+          <Image src={placeholder} alt='card image' className='rounded-md w-full h-full object-cover' />
+        </div>
+
+        <div className='bg-white w-[99%] mx-auto h-fit flex flex-col p-1 mt-1'>
+          <div className='w-[90%] mx-auto bg-white mt-1'>
+            <div>
+              <input
+                className='input w-[80%] mx-auto input-bordered join-item inset bg-gray-200 '
+                placeholder='Search'
+                onClick={() => setSearchParams(true)}
+              />
+            </div>
+            <select
+              className='select bg-gray-300 select-bordered join-item hover:bg-green-400'
+              onChange={(e) => setFilterOption(e.target.value)}
+            >
+              <option value='ALL'>All</option>
+              <option value='STUDENTS'>Students</option>
+              <option value='COURSES'>Courses</option>
+              <option value='INSTRUCTORS'>Instructors</option>
+              <option value='PAYMENTS'>Payments</option>
+            </select>
+            <div className='indicator'>
+              <button className='btn btn-ghost text-black hover:bg-green-400 join-item'>
+                <FaSearch size={20} />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      {searchParams && (
+        <div className='w-[80%] mx-auto mb-1 mt-1 bg-gray-200 h-[400px] card overflow-y-scroll p-2'>
+          {filteredData().map((item: any, index: number) => (
+            <div key={item.id || item.course_id || item.student_id || item.instructor_id}>
+              {item.course_name && <h3>{item.course_name}</h3>}
+              {item.course_instructor && <p>{item.course_instructor}</p>}
+              {item.student_name && <div className='flex flex-row p-2 bg-green-200 w-[90%] mx-auto mt-1 justify-evenly rounded-md '>
+                <p>{item.student_name}</p>
+                </div>}
+              {item.instructor_name && (
+                <div className='flex flex-row p-2 bg-green-200 w-[90%] mx-auto mt-1 justify-evenly rounded-md ' >
+                  <p>{item.instructor_name}</p>
+                  <p><Rating rating={item.instructor_rating} /></p>
+                </div>
+              )}
+              {item.payer && <p>{item.payer}</p>}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Page;
+
+
+
+
+
+
+
+
+
+  {/*<option value='ALL'>ALL</option>*/}
+
+  {/*
+     useEffect(() => {
     let newData: AllData[] = [];
 
     if (filterOption.toUpperCase() === 'ALL') {
@@ -73,61 +164,5 @@ const Page = () => {
     }
 
     setSearchParamData(newData);
-  }, [filterOption]);
-
-  return (
-    <div className='overflow-y-scroll h-screen'>
-      <div className='card bg-gray-200 w-[80%] p-2 mx-auto h-[540px] my-auto'>
-        <div className='h-[400px] w-[99%] mx-auto rounded-md'>
-          <p className='absolute pt-5 bg-gray-100 p-2 rounded-tl-md rounded-br-md'>Information desk</p>
-          <Image src={placeholder} alt='card image' className='rounded-md w-full h-full object-fit' />
-        </div>
-
-        <div className='bg-white w-[99%] mx-auto h-fit flex flex-col p-1 mt-1'>
-          <div className='w-[90%] mx-auto bg-white mt-1'>
-            <div>
-              <input
-                className='input w-[80%] mx-auto input-bordered join-item inset bg-gray-200 '
-                placeholder='Search'
-                onClick={() => {
-                  setSearchParams(true);
-                }}
-              />
-            </div>
-            <select
-              className='select bg-gray-300 select-bordered join-item hover:bg-green-400'
-              onChange={(e) => setFilterOption(e.target.value)}
-            >
-              <option value='ALL'>ALL</option>
-              <option value='STUDENTS'>students</option>
-              <option value='COURSES' onClick={(e:any)=>{
-                e.preventDefault();
-                {courses.length==0&&setcourses(data.courses)}
-                alert(courses)
-              }} >courses</option>
-              <option value='INSTRUCTORS'>instructors</option>
-              <option value='PAYMENTS'>payments</option>
-            </select>
-            <div className='indicator'>
-              <button className='btn btn-ghost text-black hover:bg-green-400 join-item'>
-                <FaSearch size={20} />
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-      {searchParams && (
-        <div className='w-[80%] mx-auto mb-1 mt-1 bg-gray-200 h-[400px] card overflow-y-scroll p-2'>
-          {courses.length!==0&&<>{searchParamData.map((item, index) => (
-            <div key={index}>
-              <h3>{item.type}</h3>
-              <pre>{JSON.stringify(item.data, null, 2)}</pre>
-            </div>
-          ))}</>}
-        </div>
-      )}
-    </div>
-  );
-};
-
-export default Page;
+  }, [filterOption]);  
+  */}
