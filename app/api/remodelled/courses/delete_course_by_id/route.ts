117097e154b6b753739b3cedc5917984161ab8a7
thomas <davincideveloper01@gmail.com>
2024-10-18 13:13:33 +0300

    18/10/2024.2

--- app/api/remodelled/courses/delete_course_by_id/route.ts
+++ app/api/remodelled/courses/delete_course_by_id/route.ts
@@ -1,7 +1,7 @@
 import { sql } from "@vercel/postgres";
 import { NextRequest,NextResponse } from "next/server";
 
-export default async function DELETE(req:NextRequest){
+export  async function DELETE(req:NextRequest){
     try {
         const {id} = await req.json()
         const response = await sql `
