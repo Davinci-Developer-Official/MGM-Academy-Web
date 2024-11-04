4bb3a39e0f45a9b8125439b72df264d356253e03
thomas <davincideveloper01@gmail.com>
2024-11-05 02:46:27 +0300

    05/11/2024.1

--- app/api/remodelled/courses/get_course_by_id/route.ts
+++ app/api/remodelled/courses/get_course_by_id/route.ts
@@ -1,13 +1,26 @@
+
 import { sql } from "@vercel/postgres";
-import { NextRequest,NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export  async function GET(req:NextRequest){
+export  async function GET(req: NextRequest) {
     try {
-        const id = await req.json()
-        const response = await sql `SELECT * FROM Courses where course_id=${id} `;
-        return NextResponse.json(response.rows);
-    } catch (error) {
-        return NextResponse.json({error:error})
-    }
+        const { searchParams } = new URL(req.url);
+        const id = searchParams.get("id"); // Extract the 'id' from query parameters
 
+        if (id=="") {
+            return NextResponse.json({ error: "Chapter ID is required" }, { status: 400 });
+        }
+        if(id!==null){
+            const response = await sql`
+            SELECT * FROM courses WHERE course_id = ${id} ;
+            `
+        console.log(id)
+        console.log(response.rows)
+        return NextResponse.json(response.rows);
+        }
+
+        
+    } catch (error:any) {
+        return NextResponse.json({ error: error.message }, { status: 500 });
+    }
 }
--- app/render3/page.tsx
+++ app/render3/page.tsx
@@ -1,29 +1,29 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import { FaCaretDown, FaCaretUp, FaInfoCircle, FaMinus, FaPlus } from 'react-icons/fa';
 import data from "./data.json";
 import Image from 'next/image';
 
-interface Topic {
-  unit: string;
-  subtopics: string[];
+
+interface Courses{
+  id:string
+  cover:string;
+  title:string;
+  description:string;
+  instructor_id:string;
+  created:string
+}
+interface Chapters{
+  id:string;
+  course_id:string;
+  title:string;
+  description:string;
+  order:string;
+  created:string;
+  file:string;
+  video:string
 }
 
-interface Course {
-  title: string;
-  courseRequirements: string[];
-  unitCode: string;
-  category: string;
-  topics: Topic[];
-  instructor: {
-    name: string;
-    photo: string;
-    role: string;
-    contactTip: string;
-  };
-  intro: string;
-  overview: string;
-}
 
 function Page() {
   const [expandedTopics, setExpandedTopics] = useState<{ [key: number]: boolean }>({});
@@ -34,10 +34,35 @@ function Page() {
       [index]: !prev[index]
     }));
   };
+  
 
   // Find the course with unitCode 'GS104'
-  const course: Course | undefined = data.courses.find(course => course.unitCode === 'GS104');
-
+  const [course,setCourse]  = useState<Courses|null>(null);
+  const[chapters,setChapters]= useState<Chapters[]>([])
+  const[error,setError]=useState()
+  const[arrCourse,setArrCourse]=useState<Courses[]>([])
+  const[loading,setLoading]=useState(false)
+  const used_id = '21feb3ce-14ce-497c-99c1-a2fd9a4b772f'
+  useMemo(()=>{
+    const fetchCourses = async () => {
+      try {
+        const response = await fetch(`/api/remodelled/courses/get_course_by_id?id=${used_id}`);
+        if (!response.ok) {
+          throw new Error('Failed to fetch courses');
+        }
+        const data = await response.json();
+        //alert(JSON.stringify(data))
+        setCourse(data);
+        setArrCourse(data)
+      } catch (err: any) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchCourses()
+  },[])
+  
   if (!course) {
     return <div>Course not found</div>;
   }
@@ -46,16 +71,16 @@ function Page() {
     <div className='w-full h-full p-2 background'>
       <div className='w-full mb-8'>
         <div className='w-full flex flex-row p-1 justify-evenly'>
-          <div className='flex flex-row justify-left bg-gray-200 cursor-pointer w-[200px] my-auto tooltip tooltip-open tooltip-bottom p-2 mb-2' data-tip={course.instructor.contactTip}>
+          <div className='flex flex-row justify-left bg-gray-200 cursor-pointer w-[200px] my-auto tooltip tooltip-open tooltip-bottom p-2 mb-2' data-tip={course.instructor_id}>
             <div className="avatar online">
               <div className="w-14 rounded-full">
-                <Image src={course.instructor.photo} alt="Instructor" />
+                <Image src={course.cover} alt="Instructor" width={300} height={300} />
               </div>
             </div>
             <div className='flex flex-col w-full text-center'>
-              <p className='text-sm font-bold pt-2'>{course.instructor.name}</p>
+              <p className='text-sm font-bold pt-2'>{course.instructor_id}</p>
               <p className='text-sm font-bold pt-2 flex flex-row p-1 justify-between'>
-                <span>{course.instructor.role}</span> <FaInfoCircle />
+                <span>{course.id}</span> <FaInfoCircle />
               </p>
             </div>
           </div>
@@ -65,17 +90,19 @@ function Page() {
         </div>
 
         <div className='p-2'>
-          <section className='p-2 text-center flex flex-col mt-2 card'>
-            <p className='text-xl font-bold underline text-center text-blue-800'>Introduction</p>
-            <p className='font-bold text-lg card-body'>{course.intro}</p>
+          {arrCourse.map(({index,data}:any)=>(
+            <section key={data.id} className='p-2 text-center flex flex-col mt-2 card'>
+            <p className='text-xl font-bold underline text-center text-blue-800'>Title</p>
+            <p className='font-bold text-lg card-body'>{data.title}</p>
             <p className='text-xl font-bold underline text-center text-blue-800'>Overview</p>
-            <p className='font-semibold card-body text-lg'>{course.overview}</p>
+            <p className='font-semibold card-body text-lg'>{data.description}</p>
           </section>
+          ))}
         </div>
 
         <div className='text-xl font-bold underline text-center text-blue-800'>Topics/Chapters</div>
         <div>
-          {course.topics.map((topic, index) => (
+          {chapters.map((topic, index) => (
             <div key={index} className='w-[90%] mx-auto h-fit mt-2 bg-white'>
               <button 
                 className='btn btn-ghost rounded-lg w-full justify-between hover:text-blue-900 hover:underline'
@@ -85,12 +112,13 @@ function Page() {
                 }}
               >
                 <p>Chapter : {index + 1}</p>
-                <p>{topic.unit}</p>
+                <p>{topic.title}</p>
                 {expandedTopics[index] ? <FaMinus /> : <FaPlus />}
               </button>
+              {/*subchapters*/}
               {expandedTopics[index] && (
                 <div className='horizontal-scroll-container w-[88%] mx-auto'>
-                  {topic.subtopics.map((subtopic, subIndex) => (
+                  {chapters.map((subtopic, subIndex) => (
                     <div 
                       key={subIndex}
                       className='card bg-gray-300 w-fit h-fit p-2 ml-2 tooltip'
@@ -101,15 +129,15 @@ function Page() {
                         className='card-title pt-2 hover:text-blue-800 cursor-pointer' 
                         htmlFor={`modal_${index}_${subIndex}`}
                       >
-                        {subtopic}
+                        {subtopic.description}
                       </label>
 
                       {/* The button to open modal */}
                       <input type="checkbox" id={`modal_${index}_${subIndex}`} className="modal-toggle" />
                       <div className="modal" role="dialog">
                         <div className="modal-box">
-                          <h3 className="text-lg font-bold">{subtopic}</h3>
-                          <p className="py-4">Details about {subtopic} will be here.</p>
+                          <h3 className="text-lg font-bold">{subtopic.title}</h3>
+                          <p className="py-4">Details about {subtopic.video} will be here.</p>
                         </div>
                         <label className="modal-backdrop" htmlFor={`modal_${index}_${subIndex}`}>Close</label>
                       </div>
