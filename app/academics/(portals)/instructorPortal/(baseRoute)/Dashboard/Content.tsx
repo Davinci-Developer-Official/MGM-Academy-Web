8768312659708e67669c357690e3fceb4999180d
Tom <tom@Tom>
2024-12-22 14:15:38 +0300

    22-12-2024.1

--- app/academics/(portals)/instructorPortal/(baseRoute)/Dashboard/Content.tsx
+++ app/academics/(portals)/instructorPortal/(baseRoute)/Dashboard/Content.tsx
@@ -4,7 +4,7 @@ export default function Content(){
     return(
     <div className='w-[99%] mx-auto bg-red-400 h-full p-2 overflow-y-auto ' >
     {/*card*/}
-    <div className='h-[200px] w-full bg-gray-200   ' >
+    <div className='h-[150px] p-2  w-full bg-gray-200   ' >
       <div>profile</div>
       <div>finances</div>
       <div>notifications</div>
