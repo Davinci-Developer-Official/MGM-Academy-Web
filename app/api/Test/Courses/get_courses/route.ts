d1a771e862e90dfb171c7ca4ee82a488e874e396
Davinci-Developer-Official <davincideveloper01@gmail.com>
2025-01-31 12:30:47 +0300

    31/01/2025.2

---
+++ app/api/Test/Courses/get_courses/route.ts
@@ -0,0 +1,13 @@
+import { NextRequest,NextResponse } from "next/server";
+import { sql } from "@vercel/postgres";
+
+export async function GET(){
+    try {
+        const response = await sql `SELECT*FROM Courses`;
+        console.log(response.rows)
+        return NextResponse.json(response.rows,{status:200});
+    } catch (error) {
+        console.log({error : 'The issue is' + error});
+        return NextResponse.json(error,{status:404})
+    }
+}
